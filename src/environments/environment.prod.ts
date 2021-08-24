1adc49d8a4f09460ed9547623698bfab37b3edd8
rameez jawaid <rameez.jawaid@yayvo.com>
2021-08-24 15:27:16 +0500

    base url changes, http from https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // base_url: 'http://datacollection.mynaghi.com/apis'
-  base_url: 'https://cdc.mynaghi.com/apis'
+  base_url: 'http://cdc.mynaghi.com/apis'
 };
